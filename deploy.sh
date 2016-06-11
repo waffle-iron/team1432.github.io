33ac33e2bcf81db797c83722f067b241e8a9041b
Caleb Eby <caleb.eby01@gmail.com>
2016-06-11 09:04:24 -0700

    only build source branch

--- deploy.sh
+++ deploy.sh
@@ -40,10 +40,13 @@ function doCompile {
 echo Travis Branch:
 echo $TRAVIS_BRANCH
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false"]; then
     echo "Skipping deploy; just doing a build."
     doCompile
     exit 0
+fi
+if [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+  exit 0
 fi
 
 # Save some useful information
