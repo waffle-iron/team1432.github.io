21be7e5350d4e3f81614c9b58a2acd73847b369d
Caleb Eby <caleb.eby01@gmail.com>
2016-06-29 08:45:32 -0700

    hi

--- deploy.sh
+++ deploy.sh
@@ -88,6 +88,7 @@ git add .
 #echo '$ git commit -m "Deploy to GitHub Pages: '${SHA}'"'
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
+
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 #ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 #ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
